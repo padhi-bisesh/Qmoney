56ce9bc9105535bd67f225269872dd096e645c1d
padhi-bisesh <bkp10@iitbbs.ac.in>
2020-06-02 05:31:18 +0000

    ME_QMONEY_MODULE_CALCULATIONS

--- qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
+++ qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
@@ -108,13 +108,9 @@ public class PortfolioManagerApplication {
   public static List<AnnualizedReturn> dtoReturn2(String[] args,List<PortfolioTrade> trades) {
     List<AnnualizedReturn> stocks = new ArrayList<AnnualizedReturn>();
     for (PortfolioTrade t:trades) {
-      String uri = null;
-      do{
-        uri = "https://api.tiingo.com/tiingo/daily/" + t.getSymbol() + "/prices?startDate="
-            + t.getPurchaseDate().toString() + "&endDate=" + args[1] 
-            + "&token=617816cec9dd9cf627651339b4c7a7775e7eb58b";
-        if()
-      }while();
+      String uri = "https://api.tiingo.com/tiingo/daily/" + t.getSymbol() + "/prices?startDate="
+          + t.getPurchaseDate().toString() + "&endDate=" + args[1] 
+          + "&token=617816cec9dd9cf627651339b4c7a7775e7eb58b";
       RestTemplate rt = new RestTemplate();
       TiingoCandle[] compStocks = rt.getForObject(uri, TiingoCandle[].class);
       int upperIndex = compStocks.length - 1;
